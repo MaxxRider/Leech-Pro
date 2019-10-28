5c6f0a110b4e63adf557dfa060e6054df496af04
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-10-28 21:05:32 +0530

    fix another derp

--- plugins/extract_link_from_message.py
+++ plugins/extract_link_from_message.py
@@ -11,16 +11,12 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-import os
-import re
-from plugins.magnetic_link_regex import extract_info_hash_from_ml
-
 
 def extract_link(message):
     custom_file_name = None
     url = None
     if "|" in message.text:
-        url, custom_file_name = text.split("|")
+        url, custom_file_name = message.text.split("|")
         url = url.strip()
         custom_file_name = custom_file_name.strip()
     elif message.entities is not None:
