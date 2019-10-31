00928f39072db16324c58b5346cd9af394f84540
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-10-31 22:18:58 +0530

    add folder uploads

--- plugins/upload_to_tg.py
+++ plugins/upload_to_tg.py
@@ -14,6 +14,8 @@ LOGGER = logging.getLogger(__name__)
 
 import os
 import time
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from plugins.display_progress import progress_for_pyrogram
 
 
@@ -29,16 +31,34 @@ async def upload_to_tg(message, local_file_name):
     caption_str += "'>"
     caption_str += "Here is the file to the link you sent"
     caption_str += "</a>"
+    if os.path.isdir(local_file_name):
+        directory_contents = os.listdir(local_file_name)
+        new_m_esg = await message.reply_text(
+            "Found {} files".format(len(directory_contents)),
+            quote=True
+        )
+        for single_file in directory_contents:
+            # recursion: will this FAIL somewhere?
+            await upload_to_tg(new_m_esg, single_file)
+    else:
+        await upload_single_file(message, local_file_name, caption_str)
+
+
+async def upload_single_file(message, local_file_name, caption_str):
     start_time = time.time()
     try:
         if local_file_name.upper().endswith(("MKV", "MP4", "WEBM")):
+            metadata = extractMetadata(createParser(local_file_name))
+            duration = 0
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
             # send video
             await message.reply_video(
                 video=local_file_name,
                 # quote=True,
                 caption=caption_str,
                 parse_mode="html",
-                # duration=,
+                duration=duration,
                 # width=,
                 # height=,
                 # thumb=,
@@ -53,15 +73,25 @@ async def upload_to_tg(message, local_file_name):
                 )
             )
         elif local_file_name.upper().endswith(("MP3", "M4A", "FLAC", "WAV")):
+            metadata = extractMetadata(createParser(local_file_name))
+            duration = 0
+            title = ""
+            artist = ""
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
+            if metadata.has("title"):
+                title = metadata.get("title")
+            if metadata.has("artist"):
+                artist = metadata.get("artist")
             # send audio
             await message.reply_audio(
                 audio=local_file_name,
                 # quote=True,
                 caption=caption_str,
                 parse_mode="html",
-                # duration=,
-                # performer=,
-                # title=,
+                duration=duration,
+                performer=artist,
+                title=title,
                 # thumb=,
                 disable_notification=True,
                 reply_to_message_id=message.reply_to_message.message_id,
@@ -91,6 +121,4 @@ async def upload_to_tg(message, local_file_name):
             )
     except Exception as e:
         await message.edit_text("**FAILED**\n" + str(e))
-    else:
-        await message.delete()
     os.remove(local_file_name)
