e15170d71db92a84ce66e2fc9338179168d2326a
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-11-01 10:38:59 +0530

    add moar sleep

--- plugins/upload_to_tg.py
+++ plugins/upload_to_tg.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-
+import asyncio
 import os
 import time
 from hachoir.metadata import extractMetadata
@@ -50,6 +50,7 @@ async def upload_to_tg(message, local_file_name, from_user):
 
 
 async def upload_single_file(message, local_file_name, caption_str):
+    await asyncio.sleep(5)
     start_time = time.time()
     try:
         if local_file_name.upper().endswith(("MKV", "MP4", "WEBM")):
