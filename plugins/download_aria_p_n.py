3a4c63e8cb737c9cf7c18e2eb1a5f8ba18dac632
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-11-01 11:58:58 +0530

    Heroku: read only file system

--- plugins/download_aria_p_n.py
+++ plugins/download_aria_p_n.py
@@ -65,10 +65,10 @@ async def aria_start():
 
 def add_magnet(aria_instance, magnetic_link, c_file_name):
     options = None
-    if c_file_name is not None:
-        options = {
-            "dir": c_file_name
-        }
+    # if c_file_name is not None:
+    #     options = {
+    #         "dir": c_file_name
+    #     }
     try:
         download = aria_instance.add_magnet(
             magnetic_link,
@@ -82,10 +82,10 @@ def add_magnet(aria_instance, magnetic_link, c_file_name):
 
 def add_url(aria_instance, text_url, c_file_name):
     options = None
-    if c_file_name is not None:
-        options = {
-            "dir": c_file_name
-        }
+    # if c_file_name is not None:
+    #     options = {
+    #         "dir": c_file_name
+    #     }
     uris = [text_url]
     # Add URL Into Queue
     try:
