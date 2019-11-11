082027f71bc9232f1b53277df1b153f01a4e8644
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-11-11 18:21:34 +0530

    some string fixes

--- plugins/upload_to_tg.py
+++ plugins/upload_to_tg.py
@@ -59,14 +59,20 @@ async def upload_to_tg(message, local_file_name, from_user):
             LOGGER.info("TODO")
             d_f_s = humanbytes(os.path.getsize(local_file_name))
             i_m_s_g = await message.reply_text(
-                "Telegram does not support uploading this file."
-                f"Detected File Size: {d_f_s} 😡"
-                "🤖 trying to split the files 🌝🌝🌚"
+                "Telegram does not support uploading this file.\n"
+                f"Detected File Size: {d_f_s} 😡\n"
+                "\n🤖 trying to split the files 🌝🌝🌚"
             )
             splitted_dir = await split_large_files(local_file_name)
             totlaa_sleif = os.listdir(splitted_dir)
             totlaa_sleif.sort()
+            number_of_files = len(totlaa_sleif)
             LOGGER.info(totlaa_sleif)
+            await i_m_s_g.edit_text(
+                f"Detected File Size: {d_f_s} 😡\n"
+                f"<code>{local_file_name}</code> splitted into {number_of_files} files.\n"
+                "trying to upload to Telegram, now ..."
+            )
             for le_file in totlaa_sleif:
                 # recursion: will this FAIL somewhere?
                 await upload_to_tg(
