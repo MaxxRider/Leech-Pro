ec9ae8f683d650cf213a19669250145baf96941f
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-11-16 20:37:40 +0530

    maybe this? 🤔😆😆😐

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -21,6 +21,7 @@ from tobrot import (
     ARIA_TWO_STARTED_PORT,
     MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START,
     AUTH_CHANNEL,
+    DOWNLOAD_LOCATION,
     EDIT_SLEEP_TIME_OUT
 )
 
@@ -31,7 +32,7 @@ async def aria_start():
     aria2_daemon_start_cmd.append("aria2c")
     # aria2_daemon_start_cmd.append("--allow-overwrite=true")
     aria2_daemon_start_cmd.append("--daemon=true")
-    # aria2_daemon_start_cmd.append(f"--dir={Config.TMP_DOWNLOAD_DIRECTORY}")
+    aria2_daemon_start_cmd.append(f"--dir={DOWNLOAD_LOCATION}")
     # TODO: this does not work, need to investigate this.
     # but for now, https://t.me/TrollVoiceBot?start=858
     aria2_daemon_start_cmd.append("--enable-rpc")
@@ -68,10 +69,10 @@ async def aria_start():
 
 def add_magnet(aria_instance, magnetic_link, c_file_name):
     options = None
-    if c_file_name is not None:
-        options = {
-            "dir": c_file_name
-        }
+    # if c_file_name is not None:
+    #     options = {
+    #         "dir": c_file_name
+    #     }
     try:
         download = aria_instance.add_magnet(
             magnetic_link,
@@ -85,10 +86,10 @@ def add_magnet(aria_instance, magnetic_link, c_file_name):
 
 def add_url(aria_instance, text_url, c_file_name):
     options = None
-    if c_file_name is not None:
-        options = {
-            "dir": c_file_name
-        }
+    # if c_file_name is not None:
+    #     options = {
+    #         "dir": c_file_name
+    #     }
     uris = [text_url]
     # Add URL Into Queue
     try:
