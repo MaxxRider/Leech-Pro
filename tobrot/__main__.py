c2c32a1964a809b1a70993464587d926fc7096ce
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-12-28 12:44:37 +0530

    add /upload and /exec command. remove aria2c logging pools

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -9,6 +9,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 import os
@@ -25,8 +26,17 @@ from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
 
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
 from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f
-from tobrot.plugins.status_message_fn import status_message_f, cancel_message_f, exec_message_f
+from tobrot.plugins.status_message_fn import (
+    status_message_f,
+    cancel_message_f,
+    exec_message_f,
+    upload_document_f
+)
 from tobrot.plugins.call_back_button_handler import button
+from tobrot.plugins.custom_thumbnail import (
+    save_thumb_nail,
+    clear_thumb_nail
+)
 
 
 if __name__ == "__main__" :
@@ -77,6 +87,12 @@ if __name__ == "__main__" :
         filters=Filters.command(["rename"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(rename_message_handler)
+    #
+    upload_document_handler = MessageHandler(
+        upload_document_f,
+        filters=Filters.command(["upload"]) & Filters.chat(chats=AUTH_CHANNEL)
+    )
+    app.add_handler(upload_document_handler)
 
     help_text_handler = MessageHandler(
         help_message_f,
@@ -100,5 +116,17 @@ if __name__ == "__main__" :
         button
     )
     app.add_handler(call_back_button_handler)
+    #
+    save_thumb_nail_handler = MessageHandler(
+        save_thumb_nail,
+        filters=Filters.command(["savethumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+    )
+    app.add_handler(save_thumb_nail_handler)
+    #
+    clear_thumb_nail_handler = MessageHandler(
+        clear_thumb_nail,
+        filters=Filters.command(["clearthumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+    )
+    app.add_handler(clear_thumb_nail_handler)
     #
     app.run()
