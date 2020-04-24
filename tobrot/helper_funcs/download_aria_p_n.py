7f04413edf214d15a9a89f82420bf59dd898c60a
Shrimadhav U K <shrimadhavuk@gmail.com>
2020-04-24 07:05:31 +0530

    Fix formatting for torrent download status

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -206,13 +206,18 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 except:
                     pass
                 #
-                msg = f"\n<b>Downloading File:</b> `{downloading_dir_name}`"
-                msg += f"\n<b>Progress:</b> {file.progress_string()} of {file.total_length_string()}"
-                msg += f"\n<b>Speed:</b> {file.download_speed_string()} ETA: {file.eta_string()}"
+                msg = f"\nDownloading File: `{downloading_dir_name}`"
+                msg += f"\nSpeed: {file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼"
+                msg += f"\nProgress: {file.progress_string()}"
+                msg += f"\nTotal Size: {file.total_length_string()}"
+
                 if is_file is None :
                    msg += f"\n<b>Connections:</b> {file.connections}"
                 else :
                    msg += f"\n<b>Info:</b>[ P : {file.connections} || S : {file.num_seeders} ]"
+
+                # msg += f"\nStatus: {file.status}"
+                msg += f"\nETA: {file.eta_string()}"
                 msg += f"\n<code>/cancel {gid}</code>"
                 # LOGGER.info(msg)
                 if msg != previous_message:
