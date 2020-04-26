db3f0db7eb4958446b08b9dc66e6ec72ba222c84
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2020-04-26 11:25:22 +0530

    re-write extract links

--- tobrot/helper_funcs/extract_link_from_message.py
+++ tobrot/helper_funcs/extract_link_from_message.py
@@ -13,17 +13,41 @@ LOGGER = logging.getLogger(__name__)
 
 import aiohttp
 
+from tobrot import (
+    TG_OFFENSIVE_API
+)
+
 
 def extract_link(message):
     custom_file_name = None
     url = None
+
     if message is None:
         url = None
         custom_file_name = None
-    elif "|" in message.text:
-        url, custom_file_name = message.text.split("|")
-        url = url.strip()
-        custom_file_name = custom_file_name.strip()
+
+    elif message.text is not None:
+        if message.text.lower().startswith("magnet:"):
+            url = message.text.strip()
+
+        elif "|" in message.text:
+            url, custom_file_name = message.text.split("|")
+            url = url.strip()
+            custom_file_name = custom_file_name.strip()
+
+        else:
+            url = message.text.strip()
+
+    elif message.caption is not None:
+        if "|" in message.caption:
+            url, custom_file_name = message.caption.split("|")
+            url = url.strip()
+            custom_file_name = custom_file_name.strip()
+
+        else:
+            url = message.caption.strip()
+
     elif message.entities is not None:
         url = message.text
+
     return url, custom_file_name
