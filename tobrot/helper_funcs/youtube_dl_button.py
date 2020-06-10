494d66c7380e475334de613b84b9ba5a6d10ec98
onlychecking <62546780+onlychecking@users.noreply.github.com>
2020-06-10 14:02:46 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -33,9 +33,8 @@ async def youtube_dl_call_back(bot, update):
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
     #
-'''
-    current_user_id = update.message.reply_to_message.from_user.id
-    current_touched_user_id = update.from_user.id
+    current_user_id = update.message.reply_to_message.id
+    current_touched_user_id = update.id
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
@@ -44,7 +43,7 @@ async def youtube_dl_call_back(bot, update):
             cache_time=0
         )
         return False, None
-'''
+
     user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
     # create download directory, if not exist
     if not os.path.isdir(user_working_dir):
