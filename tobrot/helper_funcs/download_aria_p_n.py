883115c8a16c1e61ae09a8b415780a43138e9c6a
onlychecking <62546780+onlychecking@users.noreply.github.com>
2020-06-17 00:42:25 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -177,14 +177,12 @@ async def call_apropriate_function(
         if check_if_file is not None:
             to_upload_file = check_if_file
     #
-'''
-    if to_upload_file:
-        if CUSTOM_FILE_NAME:
-            os.rename(to_upload_file, f"{CUSTOM_FILE_NAME}{to_upload_file}")
-            to_upload_file = f"{CUSTOM_FILE_NAME}{to_upload_file}"
-        else:
-            to_upload_file = to_upload_file
-'''
+    #if to_upload_file:
+        #if CUSTOM_FILE_NAME:
+            #os.rename(to_upload_file, f"{CUSTOM_FILE_NAME}{to_upload_file}")
+            #to_upload_file = f"{CUSTOM_FILE_NAME}{to_upload_file}"
+        #else:
+            #to_upload_file = to_upload_file
     if to_upload_file:
         os.rename(to_upload_file, cstom_file_name)
         to_upload_file_name = cstom_file_name
