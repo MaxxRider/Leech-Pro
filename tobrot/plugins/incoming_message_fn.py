4f59569948680420ae3cf019da18e73cacc69c1a
onlychecking <62546780+onlychecking@users.noreply.github.com>
2020-06-17 21:27:19 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -23,7 +23,7 @@ import time
 import aria2p
 import asyncio
 from tobrot.helper_funcs.extract_link_from_message import extract_link
-from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
+from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, call_apropriate_function_g, aria_start
 from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
@@ -69,6 +69,51 @@ async def incoming_message_f(client, message):
         await i_m_sefg.edit_text("trying to download")
         # try to download the "link"
         sagtus, err_message = await call_apropriate_function(
+            aria_i_p,
+            dl_url,
+            new_download_location,
+            i_m_sefg,
+            is_zip,
+            cf_name
+        )
+        if not sagtus:
+            # if FAILED, display the error message
+            await i_m_sefg.edit_text(err_message)
+    else:
+        await i_m_sefg.edit_text(
+            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            f"<b>API Error</b>: {cf_name}"
+        )
+#
+async def incoming_gdrive_message_f(client, message):
+    """/gleech command"""
+    i_m_sefg = await message.reply_text("processing", quote=True)
+    is_zip = False
+    if len(message.command) > 1:
+        if message.command[1] == "archive":
+            is_zip = True
+    # get link from the incoming message
+    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "LEECH")
+    LOGGER.info(dl_url)
+    LOGGER.info(cf_name)
+    if dl_url is not None:
+        await i_m_sefg.edit_text("extracting links")
+        # start the aria2c daemon
+        aria_i_p = await aria_start()
+        LOGGER.info(aria_i_p)
+        current_user_id = message.from_user.id
+        # create an unique directory
+        new_download_location = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(time.time())
+        )
+        # create download directory, if not exist
+        if not os.path.isdir(new_download_location):
+            os.makedirs(new_download_location)
+        await i_m_sefg.edit_text("trying to download")
+        # try to download the "link"
+        sagtus, err_message = await call_apropriate_function_g(
             aria_i_p,
             dl_url,
             new_download_location,
