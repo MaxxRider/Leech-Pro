ae4ccdc80e3cad66fe9e0feabcd5eb884493b970
onlychecking <62546780+onlychecking@users.noreply.github.com>
2020-06-17 21:54:32 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -30,7 +30,7 @@ from tobrot import (
 from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
 
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
-from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_handler_f
+from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_f
 from tobrot.plugins.status_message_fn import (
     status_message_f,
     cancel_message_f,
