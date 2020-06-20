c61ecad6503837a227c891a273c2aa1d3626fe68
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-20 23:00:50 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -47,6 +47,8 @@ async def incoming_message_f(client, message):
     if len(message.command) > 1:
         if message.command[1] == "archive":
             is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
     # get link from the incoming message
     dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "LEECH")
     LOGGER.info(dl_url)
@@ -74,7 +76,8 @@ async def incoming_message_f(client, message):
             new_download_location,
             i_m_sefg,
             is_zip,
-            cf_name
+            cf_name,
+            is_unzip
         )
         if not sagtus:
             # if FAILED, display the error message
@@ -92,6 +95,8 @@ async def incoming_gdrive_message_f(client, message):
     if len(message.command) > 1:
         if message.command[1] == "archive":
             is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True
     # get link from the incoming message
     dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "GLEECH")
     LOGGER.info(dl_url)
@@ -119,7 +124,8 @@ async def incoming_gdrive_message_f(client, message):
             new_download_location,
             i_m_sefg,
             is_zip,
-            cf_name
+            cf_name,
+            is_unzip
         )
         if not sagtus:
             # if FAILED, display the error message
