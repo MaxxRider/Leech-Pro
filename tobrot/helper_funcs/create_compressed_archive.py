adb290fad2a3668a6724d94eae2f4207ccdbda54
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-21 00:02:18 +0530

    Update create_compressed_archive.py

--- tobrot/helper_funcs/create_compressed_archive.py
+++ tobrot/helper_funcs/create_compressed_archive.py
@@ -63,27 +63,22 @@ async def unzip_me(input_directory):
             #compressed_file_name = base_dir_name[0:(64 - suffix_extention_length)]
             #compressed_file_name += ".tar.gz"
         # fix for https://t.me/c/1434259219/13344
-        file_genertor_command = [
+        process = subprocess.Popen([
             "unzip",
-            base_dir_name,
+            "-o",
+            f"{base_dir_name}",
             "-d",
-            uncompressed_file_name
-        ]
-        process = await asyncio.create_subprocess_exec(
-            *file_genertor_command,
-            # stdout must a pipe to be accessible as process.stdout
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
+            f"{uncompressed_file_name}"],
+            stdout=subprocess.PIPE
         )
         # Wait for the subprocess to finish
-        stdout, stderr = await process.communicate()
-        e_response = stderr.decode().strip()
-        t_response = stdout.decode().strip()
+        stdout, stderr = process.communicate()
+        #e_response = stderr.decode().strip()
+        #t_response = stdout.decode().strip()
         if os.path.exists(uncompressed_file_name):
             try:
-                shutil.rmtree(input_directory)
+                os.remove(input_directory)
             except:
                 pass
             return_name = uncompressed_file_name
-    return return_name
-
+    return return_name   
