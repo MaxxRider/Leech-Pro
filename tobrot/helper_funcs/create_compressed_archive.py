1f008e90c09d11aa8d1e9dc06731ed61da659047
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-21 20:45:56 +0530

    Update create_compressed_archive.py

--- tobrot/helper_funcs/create_compressed_archive.py
+++ tobrot/helper_funcs/create_compressed_archive.py
@@ -85,7 +85,7 @@ async def unzip_me(input_directory):
             print(return_name)
     return return_name
 #
-async def unrar_me(input_directory):
+async def untar_me(input_directory):
     return_name = None
     if os.path.exists(input_directory):
         base_dir_name = os.path.basename(input_directory)
@@ -111,4 +111,29 @@ async def unrar_me(input_directory):
             return_name = uncompressed_file_name
             print(return_name)
     return return_name
-    
+#
+async def unrar_me(input_directory):
+    return_name = None
+    if os.path.exists(input_directory):
+        base_dir_name = os.path.basename(input_directory)
+        uncompressed_file_name = os.path.splitext(base_dir_name)[0]
+        os.mkdir(f"{uncompressed_file_name}")
+        process = subprocess.Popen([
+            "unrar",
+            "x",
+            f"{base_dir_name}",
+            f"{uncompressed_file_name}"],
+            stdout=subprocess.PIPE
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = process.communicate()
+        #e_response = stderr.decode().strip()
+        #t_response = stdout.decode().strip()
+        if os.path.exists(uncompressed_file_name):
+            try:
+                os.remove(input_directory)
+            except:
+                pass
+            return_name = uncompressed_file_name
+            print(return_name)
+    return return_name
