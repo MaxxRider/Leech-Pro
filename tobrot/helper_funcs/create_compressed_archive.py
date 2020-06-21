8d9f56a60397032fce7d6ab3508f9696f2e20c92
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-21 21:15:00 +0530

    Update create_compressed_archive.py

--- tobrot/helper_funcs/create_compressed_archive.py
+++ tobrot/helper_funcs/create_compressed_archive.py
@@ -117,7 +117,7 @@ async def unrar_me(input_directory):
     if os.path.exists(input_directory):
         base_dir_name = os.path.basename(input_directory)
         uncompressed_file_name = os.path.splitext(base_dir_name)[0]
-        os.mkdir(f"{uncompressed_file_name}")
+        #os.mkdir(f"{uncompressed_file_name}")
         process = subprocess.Popen([
             "unrar",
             "x",
