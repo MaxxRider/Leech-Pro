443d3dbc78ef09189cf84ef114eab49c71828951
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-22 08:54:33 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,27 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help message
     await message.reply_text(
-        """## Available Commands
-
-        * `/ytdl`: This command should be used as reply to a [supported link](https://ytdl-org.github.io/youtube-dl/supportedsites.html)
-
-        * `/leech`: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
-
-        * `/leech archive`: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
-
-        * `/gleech`: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the drive using rclone.
-
-        * [Only work with direct link for now]It is like u can add custom name as prefix of the original file name.
-        Like if your file name is `gk.txt` uploaded will be what u add in `CUSTOM_FILE_NAME` + `gk.txt`
-
-        Only works with direct link.No magnet or torrent.
-
-        And also added custom name like...
-
-        You have to pass link as 
-        `www.download.me/gk.txt | new.txt`
-
-        the file will be uploaded as `new.txt`."""
+        ""join this group forr help-- @torrentleechgdrivesupport ""
     )
 
 
