c832eef92f78010080d63084497a002b89ec28af
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-23 20:19:20 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -25,6 +25,7 @@ async def progress_for_pyrogram(
     message,
     start
 ):
+    """ generic progress display for Telegram Upload / Download status """
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
@@ -35,12 +36,12 @@ async def progress_for_pyrogram(
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
 
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        elapsed_time = time_formatter(milliseconds=elapsed_time)
+        estimated_total_time = time_formatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
-            ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
@@ -51,39 +52,39 @@ async def progress_for_pyrogram(
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
-            if not message.photo:
-                await message.edit_text(
-                    text="{}\n {}".format(
-                        ud_type,
-                        tmp
-                    )
+            await message.edit(
+                "{}\n {}".format(
+                    ud_type,
+                    tmp
                 )
-            else:
-                await message.edit_caption(
-                    caption="{}\n {}".format(
-                        ud_type,
-                        tmp
-                    )
-                )
+            )
         except:
             pass
 
 
-def humanbytes(size):
+def humanbytes(size: int) -> str:
+    """ converts bytes into human readable format """
     # https://stackoverflow.com/a/49361727/4723940
     # 2**10 = 1024
     if not size:
         return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    power = 2 ** 10
+    number = 0
+    dict_power_n = {
+        0: " ",
+        1: "Ki",
+        2: "Mi",
+        3: "Gi",
+        4: "Ti"
+    }
     while size > power:
         size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+        number += 1
+    return str(round(size, 2)) + " " + dict_power_n[number] + 'B'
 
 
-def TimeFormatter(milliseconds: int) -> str:
+def time_formatter(milliseconds: int) -> str:
+    """ converts seconds into human readable format """
     seconds, milliseconds = divmod(int(milliseconds), 1000)
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
