cf661ebee3d173542b534982a88b69c14e8db864
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-24 08:05:09 +0530

    Update download.py

--- tobrot/helper_funcs/download.py
+++ tobrot/helper_funcs/download.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) gautamajay52 | Shrimadhav U K
+
 # the logging things
 import logging
 logging.basicConfig(
@@ -14,7 +18,6 @@ import os
 import time
 import subprocess
 from datetime import datetime
-from pySmartDL import SmartDL
 from pyrogram import Client, Filters
 
 from tobrot import (
@@ -23,91 +26,51 @@ from tobrot import (
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.upload_to_tg import upload_to_gdrive
 from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function_t
-from tobrot.helper_funcs.create_compressed_archive import unzip_me
-async def down_load_media_f(client, sms):
-    message = await sms.reply_text("...", quote=True)
+from tobrot.helper_funcs.create_compressed_archive import unzip_me, unrar_me, untar_me
+
+async def down_load_media_f(client, message):
+    mess_age = await message.reply_text("...", quote=True)
     if not os.path.isdir(DOWNLOAD_LOCATION):
         os.makedirs(DOWNLOAD_LOCATION)
-    if sms.reply_to_message is not None:
+    if message.reply_to_message is not None:
         start_t = datetime.now()
         download_location = DOWNLOAD_LOCATION + "/"
         c_time = time.time()
         the_real_download_location = await client.download_media(
-            message=sms.reply_to_message,
+            mess_age=message.reply_to_message,
             file_name=download_location,
             progress=progress_for_pyrogram,
             progress_args=(
-                "trying to download", message, c_time
+                "trying to download", mess_age, c_time
             )
         )
         end_t = datetime.now()
         ms = (end_t - start_t).seconds
         print(the_real_download_location)
-        await message.reply(f"Downloaded to <code>{the_real_download_location}</code> in <u>{ms}</u> seconds")
-        gl = subprocess.Popen(['mv', f'{the_real_download_location}', '/app/'], stdout = subprocess.PIPE)
-        out = gl.communicate()
+        await mess_age.edit(f"Downloaded to <code>{the_real_download_location}</code> in <u>{ms}</u> seconds")
+        gk = subprocess.Popen(['mv', f'{the_real_download_location}', '/app/'], stdout = subprocess.PIPE)
+        out = gk.communicate()
         the_real_download_location_g = os.path.basename(the_real_download_location)
-        if len(sms.command) > 1:
-            if sms.command[1] == "unzip":
+        if len(message.command) > 1:
+            if message.command[1] == "unzip":
                 file_upload = await unzip_me(the_real_download_location_g)
                 if file_upload is not None:
-                    await upload_to_gdrive(file_upload, sms)
-            else:
-                await upload_to_gdrive(the_real_download_location_g, sms)
-        #download_location_g = os.path.basename(the_real_download_location)
-        #print(download_location_g)
-        #final_response= await call_apropriate_function_t(download_location_g, message, is_unzip, is_unrar, is_untar)
-        #LOGGER.info(final_response)
+                    g_response = await upload_to_gdrive(file_upload, mess_age)
+                    LOGGER.info(g_response)
+                    
+            elif message.command[1] == "unrar":
+                file_uploade = await unrar_me(the_real_download_location_g)
+                if file_uploade is not None:
+                    gk_response = await upload_to_gdrive(file_uploade, mess_age)
+                    LOGGER.info(gk_response)
+                    
+           elif message.command[1] == "untar":
+                file_uploadg = await untar_me(the_real_download_location_g)
+                if file_uploadg is not None:
+                    gau_response = await upload_to_gdrive(file_uploadg, mess_age)
+                    LOGGER.info(gau_response)
+        else:
+            gaut_response = await upload_to_gdrive(the_real_download_location_g, mess_age)
+            LOGGER.info(gaut_response)
     else:
-        await message.edit("Reply to a Telegram Media, to download it to local server.")
-    '''
-    elif len(sms.command) > 1:
-        start_t = datetime.now()
-        the_url_parts = " ".join(sms.command[1:])
-        url = the_url_parts.strip()
-        custom_file_name = os.path.basename(url)
-        if "|" in the_url_parts:
-            url, custom_file_name = the_url_parts.split("|")
-            url = url.strip()
-            custom_file_name = custom_file_name.strip()
-        download_file_path = os.path.join(DOWNLOAD_LOCATION, custom_file_name)
-        downloader = SmartDL(url, download_file_path, progress_bar=False)
-        downloader.start(blocking=False)
-        c_time = time.time()
-        while not downloader.isFinished():
-            total_length = downloader.filesize if downloader.filesize else None
-            downloaded = downloader.get_dl_size()
-            display_message = ""
-            now = time.time()
-            diff = now - c_time
-            percentage = downloader.get_progress() * 100
-            speed = downloader.get_speed()
-            elapsed_time = round(diff) * 1000
-            progress_str = "[{0}{1}]\nProgress: {2}%".format(
-                ''.join(["█" for i in range(math.floor(percentage / 5))]),
-                ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
-                round(percentage, 2))
-            estimated_total_time = downloader.get_eta(human=True)
-            try:
-                current_message = f"trying to download\n"
-                current_message += f"URL: {url}\n"
-                current_message += f"File Name: {custom_file_name}\n"
-                current_message += f"{progress_str}\n"
-                current_message += f"{humanbytes(downloaded)} of {humanbytes(total_length)}\n"
-                current_message += f"ETA: {estimated_total_time}"
-                if round(diff % 10.00) == 0 and current_message != display_message:
-                    await message.edit(
-                        disable_web_page_preview=True,
-                        text=current_message
-                    )
-                    display_message = current_message
-                    await asyncio.sleep(10)
-            except Exception as e:
-                LOGGER.info(str(e))
-                pass
-        if os.path.exists(download_file_path):
-            end_t = datetime.now()
-            ms = (end_t - start_t).seconds
-            #await message.edit(f"Downloaded to <code>{download_file_path}</code> in <u>{ms}</u> seconds")
-            #await upload_to_gdrive(download_file_path, message)
-    '''
+        await message.edit("Reply to a Telegram Media, to upload to the Cloud Drive.")
