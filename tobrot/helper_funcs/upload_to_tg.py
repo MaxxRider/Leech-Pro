d8f0ef5065c6176625cf37392f86da671473b2cb
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-06-26 00:45:52 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -125,7 +125,7 @@ async def upload_to_tg(
 #
 
 async def upload_to_gdrive(file_upload, message):
-    await message.edit_text("📢 Now Uploading to ☁️ cloud...")
+    await message.edit_text("🔊 Now Uploading to ☁️ cloud...")
     subprocess.Popen(('touch', 'rclone.conf'), stdout = subprocess.PIPE)
     with open('rclone.conf', 'a', newline="\n") as fole:
         fole.write("[DRIVE]\n")
