0f27d13b15f69a967726b60f67c6fd519f1ae81b
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-06-27 23:22:44 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""<b>Need Any Help? Then Contact me</b>-- @MaxxRider\n\n <b>And also don't forget to Subscribe Our Channel</b>: <a href="https://t.me/joinchat/AAAAAFfWz-izyEXi-mpiQQ>Subscribe</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
@@ -46,12 +46,12 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/MaxxPrivate"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "please use👉 @MaxxRenameFileBot",
         quote=True,
         reply_markup=reply_markup
     )
