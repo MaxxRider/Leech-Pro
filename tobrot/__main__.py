a9d6593d151c99e4902b58dc609b61a0f3fd2e72
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-07-11 12:01:18 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -119,14 +119,14 @@ if __name__ == "__main__" :
     '''
     eval_message_handler = MessageHandler(
         eval_message_f,
-        filters=Filters.command(["eval@MaxxLeechPro_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["eval"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(eval_message_handler)
     '''
     #
     rename_message_handler = MessageHandler(
         rename_message_f,
-        filters=Filters.command(["rename@MaxxLeechPro_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["rename"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(rename_message_handler)
     #
@@ -138,7 +138,7 @@ if __name__ == "__main__" :
  
     help_text_handler = MessageHandler(
         help_message_f,
-        filters=Filters.command(["help@MaxxLeechVideo_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["help"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(help_text_handler)
     #
@@ -161,13 +161,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command(["savethumbnail@MaxxLeechVideo_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["savethumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command(["clearthumbnail@MaxxLeechVideo_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["clearthumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     #
