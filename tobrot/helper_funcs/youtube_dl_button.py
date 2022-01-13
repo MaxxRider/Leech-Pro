f6a47ff5cdcef4a80c7a0689c9e6081ae97315ea
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2022-01-13 23:43:07 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -96,7 +96,7 @@ async def youtube_dl_call_back(bot, update):
     thumb_image = response_json.get("thumbnail", thumb_image)
     if tg_send_type == "audio":
         command_to_exec = [
-            "youtube-dl",
+            "yt-dlp",
             "-c",
             "--prefer-ffmpeg",
             "--extract-audio",
@@ -118,7 +118,7 @@ async def youtube_dl_call_back(bot, update):
                 break
 
         command_to_exec = [
-            "youtube-dl",
+            "yt-dlp",
             "-c",
             "--embed-subs",
             "-f",
